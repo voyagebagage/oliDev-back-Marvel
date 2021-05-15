fa9ef74dfa20f2274227a12a41712003baa1907a
Olivier Frugier <sedatif2@MacBook-Pro-de-Olivier.local>
2021-05-15 22:36:23 +0700

    w/comics

--- routes/comics.js
+++ routes/comics.js
@@ -13,5 +13,19 @@ router.get("/comics/:characterId", async (req, res) => {
     res.status(400).json({ error: error.message });
   }
 });
+
+router.get("/comics", async (res, req) => {
+  try {
+    console.log("welcom 4");
+    const limit = req.query.limit || 100;
+    const skip = req.query.skip || 0;
+    const { title } = req.query;
+    const response = await axios.get(
+      `https://lereacteur-marvel-api.herokuapp.com/comics?apiKey=${process.env.API_KEY}&limit=${limit}&skip=${skip}&title=${title}`
+    );
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
 //E_X_P_O_R_T\\
 module.exports = router;
