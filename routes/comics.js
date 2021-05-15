c43f29355d1633eae76edc49f7af0349d5c277d1
Olivier Frugier <sedatif2@MacBook-Pro-de-Olivier.local>
2021-05-15 22:44:24 +0700

    swap routes

--- routes/comics.js
+++ routes/comics.js
@@ -2,18 +2,6 @@ const express = require("express");
 const router = express.Router();
 const axios = require("axios");
 
-router.get("/comics/:characterId", async (req, res) => {
-  try {
-    console.log("W E L C O M E 3");
-    const response = await axios.get(
-      `https://lereacteur-marvel-api.herokuapp.com/comics/${req.params.characterId}?apiKey=${process.env.API_KEY}`
-    );
-    res.status(200).json(response.data);
-  } catch (error) {
-    res.status(400).json({ error: error.message });
-  }
-});
-
 router.get("/comics", async (res, req) => {
   try {
     console.log("welcom 4");
@@ -27,5 +15,18 @@ router.get("/comics", async (res, req) => {
     res.status(400).json({ error: error.message });
   }
 });
+
+router.get("/comics/:characterId", async (req, res) => {
+  try {
+    console.log("W E L C O M E 3");
+    const response = await axios.get(
+      `https://lereacteur-marvel-api.herokuapp.com/comics/${req.params.characterId}?apiKey=${process.env.API_KEY}`
+    );
+    res.status(200).json(response.data);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
+
 //E_X_P_O_R_T\\
 module.exports = router;
