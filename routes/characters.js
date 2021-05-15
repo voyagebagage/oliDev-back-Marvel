dbe81ab331bd2f1ab6ca331056eb0413020c114b
Olivier Frugier <sedatif2@MacBook-Pro-de-Olivier.local>
2021-05-15 15:48:02 +0700

    w/ queries

--- routes/characters.js
+++ routes/characters.js
@@ -4,12 +4,14 @@ const axios = require("axios");
 
 router.get("/characters", async (req, res) => {
   try {
-    const limit = req.query.limit || 100;
-    const skip = req.query.skip || 0;
+    // const limit = req.query.limit || 100;
+    // const skip = 100;
     console.log("W E L C O M E2222");
+    const { limit, skip } = req.query;
     const response = await axios.get(
       `https://lereacteur-marvel-api.herokuapp.com/characters?apiKey=${process.env.API_KEY}&limit=${limit}&skip=${skip}`
     );
+
     res.status(200).json(response.data);
   } catch (error) {
     res.status(400).json({ error: error.message });
